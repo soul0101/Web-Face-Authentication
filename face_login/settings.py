af7aa73619b720d9b81c34a819fd6b4b7794992f
soul0101 <53980340+soul0101@users.noreply.github.com>
2021-03-23 20:37:08 +0530

    changes

--- face_login/settings.py
+++ face_login/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import django_heroku
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #Added
 STATICFILES_DIRS = [
